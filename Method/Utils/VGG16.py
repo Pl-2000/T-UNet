376d3ecb5d7fb3d1af7fb1ecc991991fb1410d8c
Pl-2000 <58409482+Pl-2000@users.noreply.github.com>
2023-04-17 11:07:17 +0800

    Add VGG16.py

---
+++ Method/Utils/VGG16.py
@@ -0,0 +1,115 @@
+import torch.nn as nn
+
+
+class VGG16_layer1(nn.Module):
+    def __init__(self):
+        super(VGG16_layer1, self).__init__()
+        self.nn_modules = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.nn_modules(x)
+
+
+class VGG16_layer2(nn.Module):
+    def __init__(self):
+        super(VGG16_layer2, self).__init__()
+        self.nn_modules = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.nn_modules(x)
+
+
+class VGG16_layer3(nn.Module):
+    def __init__(self):
+        super(VGG16_layer3, self).__init__()
+        self.nn_modules = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.nn_modules(x)
+
+
+class VGG16_layer4(nn.Module):
+    def __init__(self):
+        super(VGG16_layer4, self).__init__()
+        self.nn_modules = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.nn_modules(x)
+
+
+class VGG16_layer5(nn.Module):
+    def __init__(self):
+        super(VGG16_layer5, self).__init__()
+        self.nn_modules = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.nn_modules(x)
+
+
+class VGG16_layer5_with_MaxPool(nn.Module):
+    def __init__(self):
+        super(VGG16_layer5_with_MaxPool, self).__init__()
+        self.nn_modules = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+        )
+
+    def forward(self, x):
+        return self.nn_modules(x)
